cbebeac0dd360924e8a3697a291c8f80e98ca553
Denys Vuika <denys.vuika@gmail.com>
2024-03-10 14:14:25 -0400

    fix export

--- packages/core/src/events/index.tsx
+++ packages/core/src/events/index.tsx
@@ -3,4 +3,10 @@ export * from './CoreEventHandlers';
 export * from './DefaultEventHandlers';
 export * from './Events';
 export * from './RenderIndicator';
+
+export * from './EventHandlers/ConnectorRegistry';
+export * from './EventHandlers/DerivedEventHandlers';
+export * from './EventHandlers/EventHandlers';
+export * from './EventHandlers/interfaces';
+export * from './EventHandlers/isEventBlockedByDescendant';
 export * from './EventHandlers/wrapConnectorHooks';
