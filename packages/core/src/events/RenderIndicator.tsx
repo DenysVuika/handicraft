3db69da6785ac3bac0ad83dd8ccfeb691751408d
Denys Vuika <denys.vuika@gmail.com>
2024-03-15 15:41:37 -0400

    update types

--- packages/core/src/events/RenderIndicator.tsx
+++ packages/core/src/events/RenderIndicator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createPortal } from 'react-dom';
 
-export const RenderIndicator = ({ style, parentDom }: any) => {
+export const RenderIndicator = ({ style, parentDom }: any): any => {
   const indicator = (
     <div
       style={{
