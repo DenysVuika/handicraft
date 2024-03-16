dfa4ac7b03967072a87e909688fd28a054c3eb46
Denys Vuika <denys.vuika@gmail.com>
2024-03-16 19:17:42 -0400

    remove layers deps for svg

--- packages/layers/rollup.config.mjs
+++ packages/layers/rollup.config.mjs
@@ -1,9 +1,7 @@
-import image from '@svgr/rollup';
-
 import config from '../../rollup.config.mjs';
 
 export default {
   ...config,
   input: './src/index.tsx',
-  plugins: [...config.plugins, image()],
+  plugins: [...config.plugins]
 };
